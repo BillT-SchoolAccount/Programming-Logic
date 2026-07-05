e2cd9a1885029ac0f7c39762dd3a68df847c7bd7
BillT-SchoolAccount <S00609657@acad.tri-c.edu>
2026-07-05 21:46:02 +0000

    Updated Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,9 +4,27 @@ internal class NewBaseType
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome to IT 1050-Programming Logic");
-    }
+        //Introduction to class
+        //name of student
+        Console.WriteLine("My name is Bill Tarczy.");
+        //course title
+        Console.WriteLine("This is IT-1050 Programming Logic.");
+        //favorite number
+        int favnumber = 7;
+        Console.WriteLine("My favorte number is " + favnumber);
+        //favorite programming language
+        string favproglanguage = "R programming language";
+        Console.WriteLine("My favorite computer language is " + favproglanguage);
+        //number of program written
+        double numprogwritten = 2;
+        Console.WriteLine("I have written about " + numprogwritten + " partial programs before this.");
+        // i do or do not have programming experince
+        bool progexperience = false;
+        Console.WriteLine("Do I have programming experience? " + progexperience);
+    {
+        
+    }    }
+
 }
 
 internal class Program : NewBaseType
---
+++ Lab2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is Lab 2."); 
+  }
+}
