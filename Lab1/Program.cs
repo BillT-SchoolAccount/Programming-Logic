80814f59e060c45e4b9d759fc639a4a3be8af17a
BillT-SchoolAccount <S00609657@acad.tri-c.edu>
2026-07-05 20:07:26 +0000

    Updated  Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,9 +1,14 @@
 ﻿namespace Lab1;
 
-class Program
+internal class NewBaseType
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to IT 1050-Programming Logic");
     }
+}
+
+internal class Program : NewBaseType
+{
 }
--- Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff191bb4bf05b49c2efc56e4d96eb13bee3d94e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f375035c486f963ca5f9aad7fe9976b4f9b59c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
